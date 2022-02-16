5742c272ef589316eed53c9328a5c3e2853279d4
Jonathan Trattner <accounts@jdtrat.com>
2022-02-16 15:43:35 -0500

    Add utility functions for creating/copying files and folders when creating a project.

---
+++ R/utils-create_dirs.R
@@ -0,0 +1,27 @@
+# Adapted from `usethis:::create_directory()`
+create_source_directory <- function(path, source) {
+  if (fs::dir_exists(path)) {
+    return(invisible(FALSE))
+  } else if (fs::file_exists(path)) {
+    usethis::ui_stop("{usethis::ui_path(path)} exists but is not a directory.")
+  } else if (!fs::dir_exists(source)) {
+    usethis::ui_stop("{usethis::ui_path(source)} does not exist.")
+  }
+  fs::dir_copy(path = source,
+               new_path = path,
+               overwrite = FALSE)
+  usethis::ui_done("Copying {usethis::ui_path(source)} to {usethis::ui_path(path)}")
+  invisible(TRUE)
+}
+
+use_source_directory <- function(path, source, ignore = FALSE) {
+  create_source_directory(
+    path = usethis::proj_path(path),
+    source = source
+  )
+
+  if (ignore) {
+    usethis:::use_build_ignore(path)
+  }
+  invisible(TRUE)
+}
---
+++ R/utils-create_files.R
@@ -0,0 +1,40 @@
+# Modified from `usethis:::create_directory()`
+create_file <- function(path) {
+  if (fs::file_exists(path)) {
+    return(invisible(FALSE))
+  }
+  fs::file_create(path)
+  usethis::ui_done("Creating {usethis::ui_path(path)}")
+  invisible(TRUE)
+}
+
+use_file <- function(path, ignore = FALSE) {
+  create_file(usethis::proj_path(path))
+  if (ignore) {
+    usethis::use_build_ignore(path)
+  }
+  invisible(TRUE)
+}
+
+create_source_file <- function(path, source) {
+  if (fs::file_exists(path)) {
+    return(invisible(FALSE))
+  }
+  fs::file_copy(path = source,
+               new_path = path,
+               overwrite = FALSE)
+  usethis::ui_done("Copying {usethis::ui_path(source)} to {usethis::ui_path(path)}")
+  invisible(TRUE)
+}
+
+use_source_file <- function(path, source, ignore = FALSE) {
+  create_source_file(
+    path = usethis::proj_path(path),
+    source = source
+  )
+
+  if (ignore) {
+    usethis::use_build_ignore(path)
+  }
+  invisible(TRUE)
+}
